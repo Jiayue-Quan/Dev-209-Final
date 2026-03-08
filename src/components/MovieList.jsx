1ab2eed46fcc1aba3f1f95138f66e4fb01445928
BuffTaco <jiayue.quan@gmail.com>
2026-03-08 01:23:19 -0800

    Fixes movie list cycling for varying length of total movies list, normalizes poster heights

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,41 @@
+import { useState } from 'react'
+
 const MovieList = ({title}) => {
-    const tempImages = ["./src/assets/1.jpg", "./src/assets/2.jpg", "./src/assets/3.jpg", "./src/assets/4.jpg"]
+    
+    const tempImages = ["./src/assets/1.jpg", "./src/assets/2.jpg", "./src/assets/3.jpg", "./src/assets/4.jpg", "./src/assets/5.jpg", "./src/assets/6.jpg"]
+    //the index of the first poster shown on-screen (relative to the tempImages list)
+    const [currListInd, setCurrListInd] = useState(0);
+    
+
+    const moveList = (next) => {
+        next ? 
+        setCurrListInd((currListInd + 1) % tempImages.length)
+        : setCurrListInd((currListInd - 1) % tempImages.length)
+    }
+
+    const getCurrList = () => {
+        if (currListInd < 0) {
+            return tempImages.slice(currListInd).concat(tempImages.slice(0, 4 + (currListInd)));
+        }
+        if (currListInd + 4 > tempImages.length) {
+            return tempImages.slice(currListInd).concat(tempImages.slice(0, 4 + currListInd - tempImages.length));
+        }
+        return tempImages.slice(currListInd, currListInd + 4);
+    }
+   
 
     return <>
     <h1>{title}</h1>
+    
     <li className="moviesList">
+        <input type="image" src="./src/assets/backArrow.png" onClick={() => {moveList(false)}}/>
             {
-                tempImages.map((image) => <img src={image}/>)
+                getCurrList().map((image) => <img className="moviePoster" src={image} key={image}/>)
             }
+
+        <input type="image" src="./src/assets/nextArrow.png" onClick={() => {moveList(true)}}/>
     </li>
+    
     </>
 }
 
